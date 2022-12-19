e5faa3dcc0353b6899eacb1728ad8feef6d5d4ce
Vera Timova <vera.timova@gmail.com>
2022-12-19 13:28:18 +0100

    Aggiunto google tag

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,8 +5,15 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         @vite(['resources/css/app.css', 'resources/js/app.js'])
         <title>Chiara Bonetti</title>
+    <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-3YK4L0LYYN"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
-      
+  gtag('config', 'G-3YK4L0LYYN');
+</script>
     </head>
     <body class="container p-0">
         <x-nav></x-nav>
